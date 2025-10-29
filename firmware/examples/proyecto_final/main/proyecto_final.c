e68ca3e9478f2b4fb7b8b88843fe3459f3038bf7
Josefina-Nicola <josefina.nicola@ingenieria.uner.edu.ar>
2025-10-29 17:46:34 -0300

    Terminado cálculo de ángulos con promedio de medidas

--- firmware/examples/proyecto_final/main/proyecto_final.c
+++ firmware/examples/proyecto_final/main/proyecto_final.c
@@ -25,11 +25,11 @@
  */
 
 /*==================[inclusions]=============================================*/
-#include "ADXL335.h"
 #include <stdio.h>
+#include <math.h>
+#include "ADXL335.h"
 #include "analog_io_mcu.h"
 #include "freertos/FreeRTOS.h"
-#include <math.h>
 #include "freertos/task.h"
 
 /*==================[macros and definitions]=================================*/
@@ -48,33 +48,51 @@
 
 /*==================[internal functions definition]==========================*/
 
-float CalcularAnguloX(float x_0, float x){
-	float angulo_x = 0;
-	angulo_x = atan(x/x_0) * (180.0 / 3.1416); //calculo del angulo de radianes a grados
-	return angulo_x;
+float CalcularPitch(float x, float y, float z){
+	float pitch = 0;
+	pitch = atan(y/sqrt((pow(x,2)+pow(z,2))))*180/3.1415;
+	return pitch;
 }
 
-float CalcularAnguloY(float y_0, float y){
-	float angulo_y = 0;
-	angulo_y = atan(y/y_0) * (180.0 / 3.1416); //calculo del angulo de radianes a grados
-	return angulo_y;
+float CalcularRoll(float x, float y, float z){
+	float roll = 0;
+	roll = atan(x/sqrt((pow(y,2)+pow(z,2))))*180/3.1415;
+	return roll;
 }
+
+// void calibracion(){
+
+// }
 
 /*==================[external functions definition]==========================*/
 
 void app_main(void)
 {
+	int n = 4;
+	
 	ADXL335Init();
+	
 	while(1){
-		int x = ReadXValueInt();
-		//printf("\nX: %.2f g\n", x);
-		printf("\nX: %d\n", x);
-		int y = ReadYValueInt();
-		//printf("Y: %.2f g\n", y);
-		printf("Y: %d\n", y);
-		int z = ReadZValueInt();
-		//printf("Z: %.2f g\n", z);
-		printf("Z: %d\n", z);
+		float x_prom = 0;
+		float y_prom = 0;
+		float z_prom = 0;
+		
+		for (int i = 0; i < n; i++){
+			x_prom += ReadXValue()/n;
+			y_prom += ReadYValue()/n;
+			z_prom += ReadZValue()/n;
+
+			vTaskDelay(10 / portTICK_PERIOD_MS);	// hacer función delay diez ms
+		}
+
+		float pitch = CalcularPitch(x_prom, y_prom, z_prom);
+		float roll = CalcularRoll(x_prom, y_prom, z_prom);
+		
+		printf("\nX: %.2f g\n", x_prom);
+		printf("Y: %.2f g\n", y_prom);
+		printf("Z: %.2f g\n", z_prom);
+		printf("\nPitch: %.2f\n", pitch);
+		printf("Roll: %.2f\n", roll);
 
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
