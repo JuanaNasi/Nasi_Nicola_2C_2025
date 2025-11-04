aefdeccf9cbb26434dc2c85b8702ff5c0c60ce7f
Josefina-Nicola <josefina.nicola@ingenieria.uner.edu.ar>
2025-11-04 19:23:05 -0300

    Cambio de VTaskDelay por timers, agrego lógica para que en total muestre cada 1 segundo

--- firmware/examples/proyecto_final/main/proyecto_final.c
+++ firmware/examples/proyecto_final/main/proyecto_final.c
@@ -19,6 +19,7 @@
  * |:----------:|:-----------------------------------------------|
  * | 22/10/2025 | Creación del documento	                     |
  * | 29/10/2025 | Terminación de cálculo de ángulos	             |
+ * | 04/11/2025 | Cambio de VTaskDelay por Timers	             |
  *
  * @author Juana Nasi (juananasi3009@gmail.com)
  * @author Josefina Nicola (josefina.nicola@ingenieria.uner.edu.ar)
@@ -32,11 +33,13 @@
 #include "analog_io_mcu.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-
+#include "timer_mcu.h"
 /*==================[macros and definitions]=================================*/
-
+#define MEDICION_PERIODO_US 10000
+#define MUESTREO_PERIODO_US 1000000
 /*==================[internal data declaration]==============================*/
-
+TaskHandle_t medir_task_handle = NULL;
+TaskHandle_t mostrar_task_handle = NULL;
 /*==================[internal functions declaration]=========================*/
 
 
@@ -48,6 +51,41 @@
 
 
 /*==================[internal functions definition]==========================*/
+void FuncTimer(void* param){
+    vTaskNotifyGiveFromISR(medir_task_handle, pdFALSE);
+	vTaskNotifyGiveFromISR(mostrar_task_handle, pdFALSE);
+}
+
+void Medir(int n, float *x_prom, float *y_prom, float *z_prom){
+	
+	for (int i = 0; i < n; i++){
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		*x_prom += ReadXValue()/n;
+		*y_prom += ReadYValue()/n;
+		*z_prom += ReadZValue()/n;
+	}
+}
+
+float Mostrar(int n){
+	while(1){
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+		float *x_prom = 0;
+		float *y_prom = 0;
+		float *z_prom = 0;
+
+		Medir(n, x_prom, y_prom, z_prom);
+
+		float pitch = CalcularPitch(*x_prom, *y_prom, *z_prom);
+		float roll = CalcularRoll(*x_prom, *y_prom, *z_prom);
+
+		printf("\nX: %.2f g\n", x_prom);
+		printf("Y: %.2f g\n", y_prom);
+		printf("Z: %.2f g\n", z_prom);
+		printf("\nPitch: %.2f\n", pitch);
+		printf("Roll: %.2f\n", roll);
+	}
+}
 
 float CalcularPitch(float x, float y, float z){
 	float pitch = 0;
@@ -73,29 +111,28 @@ void app_main(void)
 	
 	ADXL335Init();
 	
-	while(1){
-		float x_prom = 0;
-		float y_prom = 0;
-		float z_prom = 0;
-		
-		for (int i = 0; i < n; i++){
-			x_prom += ReadXValue()/n;
-			y_prom += ReadYValue()/n;
-			z_prom += ReadZValue()/n;
+	timer_config_t timer_medicion = {
+        .timer = TIMER_A,
+        .period = MEDICION_PERIODO_US,
+        .func_p = FuncTimer,
+        .param_p = NULL
+    };
+	TimerInit(&timer_medicion);
 
-			vTaskDelay(10 / portTICK_PERIOD_MS);	// hacer función delay diez ms
-		}
+	timer_config_t timer_muestreo = {
+        .timer = TIMER_B,
+        .period = (MUESTREO_PERIODO_US-MEDICION_PERIODO_US*n), //Deja un total de 1 segundo teniendo en cuenta el tiempo de medición
+        .func_p = FuncTimer,
+        .param_p = NULL
+    };
+	TimerInit(&timer_muestreo);
 
-		float pitch = CalcularPitch(x_prom, y_prom, z_prom);
-		float roll = CalcularRoll(x_prom, y_prom, z_prom);
-		
-		printf("\nX: %.2f g\n", x_prom);
-		printf("Y: %.2f g\n", y_prom);
-		printf("Z: %.2f g\n", z_prom);
-		printf("\nPitch: %.2f\n", pitch);
-		printf("Roll: %.2f\n", roll);
+	xTaskCreate(Medir, "Medir aceleraciones", 2048, NULL, 5, &medir_task_handle);
+	xTaskCreate(Mostrar, "Mostrar pitch y roll", 2048, NULL, 5, &mostrar_task_handle);
 
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
-	}
+	TimerStart(TIMER_A);
+	TimerStart(TIMER_B);
+
+	
 }
 /*==================[end of file]============================================*/
