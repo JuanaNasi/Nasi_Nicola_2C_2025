69a6b940c9d5b60644fe36b0363cafcf305a7561
JuanaNasi <juanitanasi@gmail.com>
2025-11-05 16:43:39 -0300

    base de funcionamiento del programa con banderitas

--- firmware/examples/proyecto_final/main/proyecto_final.c
+++ firmware/examples/proyecto_final/main/proyecto_final.c
@@ -35,55 +35,86 @@
 #include "freertos/task.h"
 #include "timer_mcu.h"
 /*==================[macros and definitions]=================================*/
+
 #define MEDICION_PERIODO_US 10000
 #define MUESTREO_PERIODO_US 1000000
+#define n 4 
+
 /*==================[internal data declaration]==============================*/
+
 TaskHandle_t medir_task_handle = NULL;
 TaskHandle_t mostrar_task_handle = NULL;
+
 /*==================[internal functions declaration]=========================*/
+
+float CalcularPitch(float x, float y, float z);
+float CalcularRoll(float x, float y, float z);
 
 
 /*==================[internal data definition]===============================*/
 
+bool flag_mostrar = false;
+float x_prom;
+float y_prom; 
+float z_prom;
 
 
 /*==================[external data definition]===============================*/
 
 
 /*==================[internal functions definition]==========================*/
-void FuncTimer(void* param){
+
+
+void FuncTimer(void *param){
     vTaskNotifyGiveFromISR(medir_task_handle, pdFALSE);
 	vTaskNotifyGiveFromISR(mostrar_task_handle, pdFALSE);
 }
 
-void Medir(int n, float *x_prom, float *y_prom, float *z_prom){
-	
-	for (int i = 0; i < n; i++){
-		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-		*x_prom += ReadXValue()/n;
-		*y_prom += ReadYValue()/n;
-		*z_prom += ReadZValue()/n;
+void Medir(void *param){
+	while(1){
+		x_prom = 0;
+		y_prom = 0;
+		z_prom = 0;
+		//ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		for (int i = 0; i < n; i++){
+			x_prom += ReadXValue()/n;
+			y_prom += ReadYValue()/n;
+			z_prom += ReadZValue()/n;
+			vTaskDelay(10 / portTICK_PERIOD_MS);
+		}		
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+		flag_mostrar = true;
 	}
+	
+	
 }
 
-float Mostrar(int n){
+void Mostrar(void *param){
 	while(1){
-		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 
-		float *x_prom = 0;
-		float *y_prom = 0;
-		float *z_prom = 0;
+		if (flag_mostrar){
+			flag_mostrar = false;
+			//ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 
-		Medir(n, x_prom, y_prom, z_prom);
+			//Medir(NULL);
 
-		float pitch = CalcularPitch(*x_prom, *y_prom, *z_prom);
-		float roll = CalcularRoll(*x_prom, *y_prom, *z_prom);
 
-		printf("\nX: %.2f g\n", x_prom);
-		printf("Y: %.2f g\n", y_prom);
-		printf("Z: %.2f g\n", z_prom);
-		printf("\nPitch: %.2f\n", pitch);
-		printf("Roll: %.2f\n", roll);
+			float pitch = CalcularPitch(x_prom, y_prom, z_prom);
+			float roll = CalcularRoll(x_prom, y_prom, z_prom);
+
+			printf("\nX: %.2f g\n", x_prom);
+			printf("Y: %.2f g\n", y_prom);
+			printf("Z: %.2f g\n", z_prom);
+			printf("\nPitch: %.2f\n", pitch);
+			printf("Roll: %.2f\n", roll);
+
+			
+			//vTaskDelay(10 / portTICK_PERIOD_MS);
+		}
+		
+		else{
+			vTaskDelay(10 / portTICK_PERIOD_MS);
+		}
 	}
 }
 
@@ -107,7 +138,6 @@ float CalcularRoll(float x, float y, float z){
 
 void app_main(void)
 {
-	int n = 4;
 	
 	ADXL335Init();
 	
@@ -117,21 +147,21 @@ void app_main(void)
         .func_p = FuncTimer,
         .param_p = NULL
     };
-	TimerInit(&timer_medicion);
+	// TimerInit(&timer_medicion);
 
-	timer_config_t timer_muestreo = {
-        .timer = TIMER_B,
-        .period = (MUESTREO_PERIODO_US-MEDICION_PERIODO_US*n), //Deja un total de 1 segundo teniendo en cuenta el tiempo de medición
-        .func_p = FuncTimer,
-        .param_p = NULL
-    };
-	TimerInit(&timer_muestreo);
+	// timer_config_t timer_muestreo = {
+    //     .timer = TIMER_B,
+    //     .period = (MUESTREO_PERIODO_US-MEDICION_PERIODO_US*n), //Deja un total de 1 segundo teniendo en cuenta el tiempo de medición
+    //     .func_p = FuncTimer,
+    //     .param_p = NULL
+    // };
+	// TimerInit(&timer_muestreo);
 
 	xTaskCreate(Medir, "Medir aceleraciones", 2048, NULL, 5, &medir_task_handle);
 	xTaskCreate(Mostrar, "Mostrar pitch y roll", 2048, NULL, 5, &mostrar_task_handle);
 
-	TimerStart(TIMER_A);
-	TimerStart(TIMER_B);
+	// TimerStart(TIMER_A);
+	// TimerStart(TIMER_B);
 
 	
 }
